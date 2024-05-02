2477854c5a11db8fa30cffbf0c47fc9cc5449fb1
Xandraya <168504466+xandraya@users.noreply.github.com>
2024-05-02 20:12:25 +0200

    add lib/ for npm compatibility

---
+++ lib/index.d.ts
@@ -0,0 +1,42 @@
+/// <reference types="node" />
+/// <reference types="node" />
+import * as pg from 'pg';
+import type { AgentOptions, IncomingHttpHeaders } from 'node:http';
+interface ScraperOptions {
+    debug?: number;
+    agentOptions?: AgentOptions;
+    pgOptions?: pg.ClientConfig;
+}
+interface ScraperRequestOptions {
+    timeout?: number;
+    headersOnly?: boolean;
+}
+export default class Scraper {
+    private _opt;
+    private _client;
+    private _store;
+    private _pub_sufix;
+    private _agent;
+    private _headers;
+    constructor(opt?: ScraperOptions);
+    bootup(): Promise<void>;
+    teardown(): Promise<void>;
+    private timeout;
+    private connectPg;
+    private createTables;
+    private loadPublicSufixes;
+    private loadCookieStore;
+    private validateCookieDate;
+    private computeDefaultPath;
+    private computeCanonHostname;
+    private domainMatch;
+    private pathMatch;
+    private parseSetCookie;
+    private computeTempStore;
+    private updateStore;
+    private parseCookie;
+    private handleHTTPError;
+    private printDebug;
+    request(host: string, path: string, cb: (data: Buffer | IncomingHttpHeaders) => void, options?: ScraperRequestOptions): Promise<number>;
+}
+export {};
